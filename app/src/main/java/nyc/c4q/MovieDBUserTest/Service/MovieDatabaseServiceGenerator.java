a641a945ded98a0b4e237e336f30a43429a19db4
bRad-95 <bryantramirez@ac.c4q.nyc>
2018-01-08 16:53:57 -0500

    Service Generator Class

---
+++ app/src/main/java/nyc/c4q/MovieDBUserTest/Service/MovieDatabaseServiceGenerator.java
@@ -0,0 +1,35 @@
+package nyc.c4q.MovieDBUserTest.Service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by c4q on 1/8/18.
+ */
+
+public class MovieDatabaseServiceGenerator {
+
+  private static final String BASE_URL = "https://api.themoviedb.org/";
+
+  private static Retrofit.Builder builder =
+      new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create());
+
+  private static Retrofit retrofit = builder.build();
+
+  private static HttpLoggingInterceptor loggingInterceptor =
+      new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+  private static OkHttpClient.Builder okhttpclient = new OkHttpClient.Builder();
+
+  public static <T> T createService(Class<T> serviceClass) {
+    if (!okhttpclient.interceptors().contains(loggingInterceptor)) {
+      okhttpclient.addInterceptor(loggingInterceptor);
+      builder.client(okhttpclient.build());
+      retrofit = builder.build();
+    }
+    return retrofit.create(serviceClass);
+  }
+
+}
