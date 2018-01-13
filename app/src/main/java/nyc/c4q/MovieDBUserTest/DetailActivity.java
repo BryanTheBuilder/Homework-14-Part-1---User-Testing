1bac5c8ef0dbb79884020bf6dc973feb7c9a1245
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-01-13 13:57:26 -0500

    detail activity

--- app/src/main/java/nyc/c4q/MovieDBUserTest/DetailActivity.java
+++ app/src/main/java/nyc/c4q/MovieDBUserTest/DetailActivity.java
@@ -2,6 +2,10 @@ package nyc.c4q.MovieDBUserTest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Shant on 1/13/2018.
@@ -9,12 +13,35 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView title;
+    TextView release;
+    TextView rating;
+    ImageView poster;
+    TextView desc;
+
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.detail_activity);
+        setVariables();
 
+    }
 
+    public void setVariables(){title = findViewById(R.id.detail_title);
+        release = findViewById(R.id.detail_release);
+        rating = findViewById(R.id.detail_rating);
+        poster = findViewById(R.id.detail_image);
+        desc = findViewById(R.id.detail_desc);
+        getIntents();
+    }
 
+    public void getIntents(){
+        Bundle extras = getIntent().getExtras();
+        release.setText(extras.getString("release"));
+        title.setText(extras.getString("title"));
+        rating.setText(Double.toString(extras.getDouble("rating")));
+        desc.setText(extras.getString("desc"));
+        Picasso.with(getBaseContext()).load(extras.getString("poster")).into(poster);
     }
 
 }
--- app/src/main/java/nyc/c4q/MovieDBUserTest/viewHolder/TVViewHolder.java
+++ app/src/main/java/nyc/c4q/MovieDBUserTest/viewHolder/TVViewHolder.java
@@ -1,6 +1,7 @@
 package nyc.c4q.MovieDBUserTest.viewHolder;
 
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import nyc.c4q.MovieDBUserTest.DetailActivity;
 import nyc.c4q.MovieDBUserTest.Models.TvResults;
 import nyc.c4q.MovieDBUserTest.R;
 
@@ -21,12 +23,25 @@ public class TVViewHolder extends RecyclerView.ViewHolder {
         tvPoster = itemView.findViewById(R.id.tv_poster);
     }
 
-    public void onBind(TvResults results) {
+    public void onBind(final TvResults results) {
         tvTitle.setText(results.getName());
-        StringBuilder fullImagePath = new StringBuilder();
+        final StringBuilder fullImagePath = new StringBuilder();
         fullImagePath.append("http://image.tmdb.org/t/p/w185/");
         fullImagePath.append(results.getPoster_path());
         Picasso.with(itemView.getContext())
                 .load(fullImagePath.toString()).into(tvPoster);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToDetailActvity = new Intent(itemView.getContext(), DetailActivity.class);
+                goToDetailActvity.putExtra("title",results.getOriginal_name());
+                goToDetailActvity.putExtra("release",results.getFirst_air_date());
+                goToDetailActvity.putExtra("rating",results.getVote_average());
+                goToDetailActvity.putExtra("desc",results.getOverview());
+                goToDetailActvity.putExtra("poster",fullImagePath.toString());
+                itemView.getContext().startActivity(goToDetailActvity);
+
+            }
+        });
     }
 }
